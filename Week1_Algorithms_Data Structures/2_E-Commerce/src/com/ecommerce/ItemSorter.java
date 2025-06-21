eac9efc60636db690edc5350abfc98456e7c0999
subhiksha siva <161231594+subhiksha23@users.noreply.github.com>
2025-06-21 17:19:54 +0530

    Delete Week1_Algorithms_Data Structures/2_E-Commerce directory

--- Week1_Algorithms_Data Structures/2_E-Commerce/src/com/ecommerce/ItemSorter.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class ItemSorter implements Comparator<Item> {
-    @Override
-    public int compare(Item a, Item b) {
-        return a.name.compareToIgnoreCase(b.name);
-    }
-}
